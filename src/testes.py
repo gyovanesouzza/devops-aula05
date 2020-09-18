04e18f6d9792cf2d0e1a5fbc77d368c46960edc9
gyovanesouzza <gyovanesouzza>
2020-09-18 20:18:28 -0300

    Correção da indentação do testes.py

--- src/testes.py
+++ src/testes.py
@@ -5,13 +5,13 @@ erroInicializar = False
 jogo = jogovelha.inicializar()
 
 if len(jogo) != 3:
-	erroInicializar = True
+    erroInicializar = True
 else:
     for linha in jogo:
         if len(linha) != 3:
-	    erroInicializar = True
-	else:
-	    for elemento in linha:
+            erroInicializar = True
+        else:
+            for elemento in linha:
                 if elemento != '.':
                     erroInicializar = True
 if erroInicializar:
